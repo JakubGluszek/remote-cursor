e65071fb901dff5ae53ef7a1c766064dc5536fe9
jacob <jacobgluszek03@gmail.com>
2023-01-11 15:25:49 +0100

    serving static html

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 async def root():
-    return {"message": "Remote cursor - init"}
+    with open("static/index.html") as f:
+        return HTMLResponse(content=f.read())
+
