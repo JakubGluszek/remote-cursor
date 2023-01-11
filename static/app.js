64c820a8e11b8c612643b9e2ea3482018c5be3b5
jacob <jacobgluszek03@gmail.com>
2023-01-11 15:48:50 +0100

    layout added

---
+++ static/app.js
@@ -0,0 +1,4 @@
+const touchpad = document.getElementById("touchpad");
+const keyboard = document.getElementById("keyboard-input");
+const leftClick = document.getElementById("left-click");
+const rightClick = document.getElementById("right-click");
