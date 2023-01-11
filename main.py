4ad4a426763d0db6026cd2658f45b019876dc343
jacob <jacobgluszek03@gmail.com>
2023-01-11 16:01:10 +0100

    websocket connection established

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
@@ -11,3 +11,9 @@ async def root():
     with open("static/index.html") as f:
         return HTMLResponse(content=f.read())
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_json()
+        print(data)
--- static/app.js
+++ static/app.js
@@ -2,3 +2,13 @@ const touchpad = document.getElementById("touchpad");
 const keyboard = document.getElementById("keyboard-input");
 const leftClick = document.getElementById("left-click");
 const rightClick = document.getElementById("right-click");
+
+const ws = new WebSocket("ws://0.0.0.0:8000/ws");
+
+leftClick.onclick = () => {
+  send({ event: "left-click" });
+};
+
+const send = (data) => {
+  ws.send(JSON.stringify(data));
+};
